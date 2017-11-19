52276efb58d25519f677406448dda9a4dedcfbf3
Rex Page <rexLpage@hotmail.com>
2017-11-18 20:34:00 -0800

    re-upload, correction
    may have uploaded an old file a few minutes ago

--- harmonicseries.lisp
+++ harmonicseries.lisp
@@ -1,14 +1,14 @@
 (include-book "doublecheck" :dir :teachpacks)
 (include-book "testing" :dir :teachpacks)
 
-(defun reciprocals (n)
+(defun h (n)
   (if (posp n)
-      (+ (reciprocals (- n 1)) (/ 1 n))
+      (+ (h (- n 1)) (/ 1 n))
       0))
 
-(defproperty reciprocals-test
+(defproperty h-test
   (n :value (random-natural))
-  (= (reciprocals n)
+  (= (h n)
      (if (= n 0)
          0
-         (+ (reciprocals (- n 1)) (/ 1 n)))))
+         (+ (h (- n 1)) (/ 1 n)))))
