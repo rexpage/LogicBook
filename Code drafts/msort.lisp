ec6bc3c691401f28437876c8b6dfa25a55712b86
Rex Page <rexLpage@hotmail.com>
2018-01-01 12:07:23 -0800

    name changes
    changed "thm" to "lemma" where relevant

--- Code drafts/msort.lisp
+++ Code drafts/msort.lisp
@@ -1,14 +1,11 @@
 (include-book "sort-predicates")
 
-(defun dmx (xys)
-  (if (consp (rest xys))      ; 2 or more elements?
-      (let* ((x (first xys))
-             (y (second xys))
-             (xsys (dmx (rest (rest xys))))
-             (xs (first xsys))
-             (ys (second xsys)))
-        (list (cons x xs) (cons y ys)))      ; dmx2
-      (list xys nil)))  ; 1 element or none  ; dmx1
+(defun dmx (xys)      ; xys = (x1 y1 x2 y2 x3 y3 ...)
+  (if (consp xys)
+      (mv-let (ys xs) ; ((y1 y2 ...)(x2 x3 ...))
+              (dmx (rest xys))
+        (mv (cons (first xys) xs) ys)) ; {dmx1}
+      (mv nil nil)))                   ; {dmx0}
 (defthm dmx-len-thm
   (mv-let (odds evns)
           (dmx xs)
@@ -32,7 +29,7 @@
 (defthm mrg-len-thm
   (= (len (mrg xs ys)) (+ (len xs) (len ys))))
 
-(defthm dmx-shortens-list-thm ; lemma to help ACL2 admit def of msort
+(defthm dmx-shortens-list-lemma ; to help ACL2 admit msort
   (implies (consp (rest xs))
            (mv-let (odds evns)
                    (dmx xs)
@@ -42,13 +39,12 @@
   (declare (xargs
             :measure (len xs)
             :hints (("Goal"
-                    :use ((:instance dmx-shortens-list-thm))))))
-  (if (consp (rest xs))   ; xs has 2 or more elements?
-      (let* ((splt (dmx xs))
-             (odds (first splt))
-             (evns (second splt)))
+                    :use ((:instance dmx-shortens-list-lemma))))))
+  (if (consp (rest xs))   ; (len xs) > 1?
+      (mv-let (odds evns) ; (len xs) > 1
+              (dmx xs)
         (mrg (msort odds) (msort evns)))    ; {msrt2}
-      xs))                ; (len xs) <= 1   ; {msrt1}                       
+      xs))                ; (len xs) <= 1     {msrt1}                       
 
 (defthm mrg-order-thm
   (implies (and (up xs) (up ys))
@@ -56,14 +52,12 @@
 (defthmd mrg-val-thm
   (iff (occurs-in e (mrg xs ys))
        (or (occurs-in e xs) (occurs-in e ys))))
-(defthm msort-order-thm-base-case
-  (up (msort xs)))
 (defthm msort-order-thm
   (up (msort xs)))
-(defthm msort-len-thm-base-case
+(defthm msort-len-lemma-base-case
   (implies (not (consp (rest xs)))
            (= (len (msort xs)) (len xs))))
-(defthm msort-len-thm-inductive-case
+(defthm msort-len-lemma-inductive-case
   (= (len (msort (cons x xs)))
      (1+ (len (msort xs)))))
 (defthm msort-len-thm
